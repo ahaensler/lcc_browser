1ce0b52dbe64dcd8c3cea4551067ce1db20f6c2c
Adrian Haensler <adrian@chronicled.com>
2023-04-05 04:37:02 -0500

    assign a node id in code example

--- bin/lcc_example.py
+++ bin/lcc_example.py
@@ -15,7 +15,7 @@ con.set_protocol(lcc)
 lcc.set_connection(con)
 
 # connect can bus
-con.connect({"device": "/dev/ttyUSB0", "uart_baudrate": 125000})
+con.connect({"device": "/dev/ttyUSB0", "uart_baudrate": 9600})
 
 # start receiving data in a background thread
 con.start()
@@ -27,6 +27,7 @@ con.set_frame_callback(log_frame)
 
 # add your application here
 # for example
+lcc.update_node_id("02.01.0D.00.00.00")
 lcc.reserve_node_alias()
 lcc.emit_event(bytes.fromhex("0123456789abcdef"))
 time.sleep(5)
